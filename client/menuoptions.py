d9818f242f053f8e8f0018cc0df366039140bb0f
ETHubbard <Hubbarderic38@gmail.com>
2025-04-16 19:30:25 -0400

    Added a file send option for the client (Option 4)
    Added the public key to be sent during registration. Server can easily
    record an pull an entire account.
    Registration now saves a local account record for client self-reference
    Currently referenced by Send key function
    
    Send key was added to menuoptions, since that is where most client
    handler functions are. Function is currently called on Login, in case a
    new key pair needed to be generated on start. Can be moved as needed.
    
    Encrytion function uses RSA + AES of a static string
    Encrypt function contains logic for requesting a user's public key.
    For testing purposes, any key matching username is returned. Intended
    for registered friends.
    
    Server handler added action cases for:
    1. storepk: Saving a public key (not called yet, for regenerated keys)
    2. getpk: Sends user key information
    3. upload: Uploading encrypted file
    
    server utils added functions for these cases.
    
    -------------------TODO--------------------------
    *Add file reader to encrypt function (client)
    *Add friends feature (client)
        *Verify friends (server) using send_pk(client)
    *Download file
        *Request download (client)
        *Send file (server)
        *Save file locally (client)
        *Notify sender of download (server)
    
    ! Both server and client are storing files outside of respective dir.
    ! Fix when packaging file/server
    
    ? Server encrypt file before storing, decrypt before transmitting
    ? More robust response for client
    ? Ensure different filetypes work

--- client/menuoptions.py
+++ client/menuoptions.py
@@ -1,18 +1,40 @@
+import datetime
 import json
 import time
 import os
 
+from Crypto.Hash import SHA256
 from cconnector import send_message, receive_message
+from keylogic import generate_key_pair
 
 # Send registration request to the server
 def send_registration_request(tls_socket):
+
+    if os.path.isfile("./keys/public.pem"):
+        os.remove("./keys/public.pem")
+        if os.path.isfile("./keys/private.pem"):
+            os.remove("./keys/private.pem")
+        
+        # Generate new pseudorandom password protected private RSA key and public RSA key
+    if not generate_key_pair(SHA256.new(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f").encode()).hexdigest()[:16]):
+        print("Key generation failed.")
+        return
+    public_key = './keys/public.pem'
+    try:
+        with open(public_key, 'r+') as key_file:
+            key_data = key_file.read()
+    except Exception as e:
+        print(f"Error opening public key file: '{e}'")
+
     """Send the registration request with user data to the server"""
     registration_data = {
         "action": "register",  # Action type
         "name": input("Enter your full name: "),
         "email": input("Enter your email address: "),
         "username": input("Choose a username: "),
-        "password": input("Choose a password (NOTE: must only contain alphanumberic characters and be 16 characters or longer): ")
+        "password": input("Choose a password (NOTE: must only contain alphanumberic characters and be 16 characters or longer): "),
+        "key": key_data
+        
     }
 
     try:
@@ -30,8 +52,15 @@ def send_registration_request(tls_socket):
                 print(f"\nRegistration failed: {data["message"]}")
             elif data["status"] == "OK":
                 print(f"\n{data["message"]}")
+            # Save account details, send server public key
+            try:    
+                with open("./client/client_account.json", 'w+') as jsonfile:
+                    json.dump(registration_data, jsonfile, indent=3)
+            except Exception as e:
+                print(f"Error writing client account data")
             # Return to client menu
             input("Returning to menu, press Enter to continue...")
+
             os.system('cls' if os.name == 'nt' else 'clear')  # Clear the screen
     except Exception as e:
         print(f"Error sending registration data: {e}")
@@ -53,6 +82,7 @@ def send_login_request(tls_socket):
         # Return to client menu
         input("Returning to menu, press Enter to continue...")
         os.system('cls' if os.name == 'nt' else 'clear')  # Clear the screen
+        send_public_key(tls_socket)
     except Exception as e:
         print(f"Error sending login data: {e}")
 
@@ -71,6 +101,34 @@ def send_exit_request(tls_socket):
             print("Invalid JSON format in response, please try again.")
             return
         # Return to main to terminate program
-        print(data["message"])
+        print(data["key"])
     except Exception as e:
         print(f"Error sending exit data: {e}")
+
+def send_public_key(tls_socket):
+    try:
+        with open('./client_account.json', 'r+') as jsonfile:
+            data = json.load(jsonfile)
+            if isinstance(data, dict) and 'name' in data:
+                name = data["name"]
+                print(name)
+    except Exception as e:
+        print(f"Error opening gathering account information: '{e}'")
+    public_key = './keys/public.pem'
+    try:
+        with open(public_key, 'r+') as key_file:
+            key_data = key_file.read()
+    except Exception as e:
+        print(f"Error opening public key file: '{e}'")
+    print(f"Public key: '{key_data}")
+    data = {
+        "action": "storepk",
+        "name": name, 
+        "key": key_data
+    }
+    message = json.dumps(data)
+    try:
+        send_message(tls_socket, message)
+    except Exception as e:
+        print(f"Error sending public key: {e}")
+    
